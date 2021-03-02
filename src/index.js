d4de808313790eacf40212e7280e4e95f3e62644
Kealoha1988 <kealohaabe1988@gmail.com>
2021-03-01 21:17:19 -0500

    commit tree c77c37be7862c8c1d8a7fcf45b0f97edcd16286f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+
+// on page load
+// fetch the images using the url above ⬆️
+// parse the response as JSON
+// add image elements to the DOM for each🤔 image in the array
+
+fetch(imgUrl).then(response => response.json).then(json => imgUrl)
+
